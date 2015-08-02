fc8c54ff9f41767d5795fff8d26bb1b15ab5e7f8
deysujan <sujandeyce@hotmail.com>
2015-08-02 10:52:39 +0200

    Added Until test project.

---
+++ Demo-Project.Tests/CustomerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Demo_project.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Demo_Project.Tests
+{
+    [TestClass]
+    public class CustomerTests
+    {
+        public List<Customer> TestCustomers
+        {
+            get
+            {
+                return new List<Customer>
+                  {
+                      new Customer{Name = "Test Person 1",Address = "Address-1"},
+                       new Customer{Name = "Test Person 2",Address = "Address-2"}
+                  };
+            }
+        }
+
+
+
+        [TestMethod]
+        public void CustomerAddressShouldBeCorrect()
+        {
+            Assert.AreEqual("Address-1", GetCustomerAddress("Test Person 1"));
+            Assert.AreEqual("Address-2", GetCustomerAddress("Test Person 2"));
+        }
+
+
+        private string GetCustomerAddress(string customerName)
+        {
+            var customer = TestCustomers.FirstOrDefault(c => c.Name == customerName);
+            if (customer != null)
+                return customer.Address;
+
+            return string.Empty;
+        }
+    }
+}
